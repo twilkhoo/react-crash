fa80c07d9f34190b16d9915bd0bc6cf0dd81682f
tejaswilkhoo <tejaswilkhoo@hotmail.com>
2021-12-21 18:23:21 -0500

    react icons

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Doctors Appointment',
+            day: 'Feb 5',
+            reminder: true,
+        },
+    
+        {
+            id: 2,
+            text: 'Soccer Practice',
+            day: 'March 5',
+            reminder: false,
+        },
+        {
+            id: 3,
+            text: 'Shopping',
+            day: 'March 5',
+            reminder: false,
+        }
+    ]);
+
+
+    return (
+        <div className="container">
+            <Header/>
+            <Tasks tasks={tasks}/>
+        </div>
+    );
 }
 
 export default App;
